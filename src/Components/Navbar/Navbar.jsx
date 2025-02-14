f2600ef3c249e29696adc408c3541b26371811a0
Muhammad Abbas <muhammadabbas100035@gmail.com>
2025-02-15 03:05:31 +0500

    css Resposive Styles Update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import underline from "../../assets/nav_underline.svg";
 import "./Navbar.css";
 import logo from "../../assets/Abbas.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 export const Navbar = () => {
   const [menu, setmenu] = useState("contact");
+  const menuRef = useRef();
+  const openmenu = () => {
+    menuRef.current.style.right = "0";
+  };
+  const closemenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
   return (
     <>
       <div className="navbar">
-        <img src={logo} alt="" className="abbas"/>
-        <ul className="nav-menu">
+        <img src={logo} alt="" className="abbas" />
+        <img
+          src={menu_open}
+          onClick={openmenu}
+          alt=""
+          className="nav-mob-open"
+        />
+        <ul ref={menuRef} className="nav-menu">
+          <img
+            src={menu_close}
+            alt=""
+            onClick={closemenu}
+            className="nav-mob-close"
+          />
           <li>
             <AnchorLink className="anchor-link" href="#home">
               <p onClick={() => setmenu("home")}>Home</p>
